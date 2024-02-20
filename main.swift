a5f884c09482194dce144d47acdc04774947d072
Anna Zhuk <anechka.zhuck@yandex.ru>
2024-02-21 01:21:36 +0300

    update script

--- main.swift
+++ main.swift
@@ -1,7 +1,55 @@
 import Foundation
 
-var name = "Анна"
-var age = 20
-var uni = "ЛЭТИ"
-var output = "Привет, меня зовут \(name), мне \(age) лет и я учусь в \(uni)"
-print(output)
+// days
+let daysInMonths: [Int] = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+// names
+let monthsNames: [String] = [
+    "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+]
+
+// generate list of tuples
+var monthsDays: [(String, Int)] = []
+for tpl in zip(monthsNames, daysInMonths) {
+    monthsDays.append(tpl)
+}
+
+// dates only
+for numberOfDays in daysInMonths {
+    print(numberOfDays)
+}
+
+print()
+
+// two arrays
+for (name, numberOfDays) in zip(monthsNames, daysInMonths) {
+    print("\(name) \(numberOfDays)")
+}
+
+print()
+
+// array of tuples
+for (name, numberOfDays) in monthsDays {
+    print("\(name) \(numberOfDays)")
+}
+
+print()
+
+// reversed
+for (name, numberOfDays) in monthsDays.reversed() {
+    print("\(name) \(numberOfDays)")
+}
+
+print()
+
+let data: (String, Int) = ("February", 1) //("October", 8)
+
+var daysToDate: Int = data.1 - 1 // because "before specified day"
+for (name, numberOfDays) in monthsDays {
+    if data.0 == name {
+        break
+    }
+    daysToDate += numberOfDays
+}
+
+print("The number of days until \(data.0) \(data.1) from the begining of the year is \(daysToDate)")
